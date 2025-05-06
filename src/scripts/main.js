1784302dcec4c8e5b1c02bb8ab75103ac41bf7ec
Doaa Ashraf <doaaelshafei1@gmail.com>
2025-05-06 08:08:54 -0200

    texh mastery task interview

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+////
+
+document.getElementById("menu-button").addEventListener("click", function () {
+  const mobileMenu = document.getElementById("mobile-menu");
+  mobileMenu.classList.toggle("hidden");
+});
+
+/////////////
+
+document.querySelectorAll(".with-slider").forEach((container) => {
+  const slider = container.querySelector(".slider-container");
+  const prevBtn = container.querySelector("button#prev-btn");
+  const nextBtn = container.querySelector("button#next-btn");
+  function getCardWidth() {
+    const firstCard = slider.querySelector(".card");
+    console.log(firstCard);
+    return firstCard ? firstCard.getBoundingClientRect().width + 24 : 0;
+  }
+
+  prevBtn.addEventListener("click", () => {
+    slider.scrollBy({ left: -getCardWidth(), behavior: "smooth" });
+  });
+
+  nextBtn.addEventListener("click", () => {
+    slider.scrollBy({ left: getCardWidth(), behavior: "smooth" });
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "450px",
+      sm: "640px",
+      md: "778px",
+      lg: "992px",
+      xl: "1024px",
+      "2xl": "1280px",
+    },
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    extend: {
+      colors: {
+        primary: "#db4444",
+      },
+    },
+  },
+  plugins: [],
+};
